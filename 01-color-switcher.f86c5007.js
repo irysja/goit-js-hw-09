230002c6d33fa04c90b46c4b20289ccdd06390aa
irysja <121570014+irysja@users.noreply.github.com>
2023-05-30 12:01:00 +0000

    Deploying to gh-pages from @ irysja/goit-js-hw-09@2dfcabc4da57d0559004193a83ff3a0e309938cf 🚀

---
+++ 01-color-switcher.f86c5007.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;function o(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`;document.body.style.backgroundColor=t}t.addEventListener("click",(function(){t.disabled=!0,n=setInterval(o,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.f86c5007.js.map
